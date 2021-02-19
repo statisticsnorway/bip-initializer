9e046f7f13e52433e019bf835bf4346ca34fb7d6
Miles Winther <mmw@ssb.no>
2021-02-19 14:59:44 +0100

    Expose basic metrics
    [STRATUS-541]
    
    Co-authored-by: lisawolderiksen <35797988+lisawolderiksen@users.noreply.github.com>
    Co-authored-by: bvestli <33520068+bvestli@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app import hello
 from app import health
@@ -7,3 +8,6 @@ app = FastAPI()
 
 app.include_router(hello.router)
 app.include_router(health.router)
+
+
+Instrumentator().instrument(app).expose(app)
