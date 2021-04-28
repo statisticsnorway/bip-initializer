dfa2124309c9573845383969a22ef3bc47e708b6
ArielKarlsen <aig@ssb.no>
2021-04-28 12:50:54 +0200

    Fix typo and add test for exposed backend w/ auth

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -48,8 +48,74 @@ def test_generate(client):
                "appType": "backend",
                "cluster": "staging-bip-app",
                "exposed": "False",
-               "IstionEndUserAuth": {
+               "istioEndUserAuth": {
                  "enabled": "False"
+               },
+               "billingProject": "ssb-stratus",
+               "image": {
+                 "repository": "eu.gcr.io/prod-bip/ssb/stratus/am-hello-world",
+                 "tag": "main-d2193bee3f24ae19e04d77826079d02cf58c0514"
+               },
+               "port": {
+                 "name": "http-am-hello-world",
+                 "containerport": 5000
+               }
+             }
+           }
+         }
+        """
+    assert response.json() == json.loads(expected_result)
+
+
+def test_generate_exposed_with_authentication(client):
+    post_data = """{
+            "name": "am-hello-world",
+            "namespace": "stratus",
+            "flux_image_tag_pattern" : "glob:main-*",
+            "cluster": "staging-bip-app",
+            "billingproject": "ssb-stratus",
+            "image_repository": "eu.gcr.io/prod-bip/ssb/stratus/am-hello-world",
+            "image_tag": "main-d2193bee3f24ae19e04d77826079d02cf58c0514",
+            "port": 5000,
+            "apptype": "backend",
+            "exposed": true,
+            "authentication": true
+        }"""
+    response = client.post(
+        "/api/v1/generate",
+        headers={"Content-Type": "application/json"},
+        json=json.loads(post_data),
+    )
+    assert response.status_code == 200
+    expected_result = """
+         {
+           "apiVersion": "helm.fluxcd.io/v1",
+           "kind": "HelmRelease",
+           "metadata": {
+             "name": "am-hello-world",
+             "namespace": "stratus",
+             "annotations": {
+               "fluxcd.io/ignore": "false",
+               "fluxcd.io/automated": "true",
+               "fluxcd.io/tag.chart-image": "glob:main-*",
+               "fluxcd.io/locked": "false"
+             }
+           },
+           "spec": {
+             "chart": {
+               "git": "ssh://git@github.com/statisticsnorway/platform-dev",
+               "ref": "master",
+               "path": "helm/charts/ssb-chart"
+             },
+             "releaseName": "am-hello-world",
+             "helmVersion": "v3",
+             "values": {
+               "name": "am-hello-world",
+               "appType": "backend",
+               "cluster": "staging-bip-app",
+               "exposed": "True",
+               "istioEndUserAuth": {
+                 "enabled": "True"
                },
                "billingProject": "ssb-stratus",
                "image": {
